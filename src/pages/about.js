7be0ae1819189225d7188da76f03f021b3228266
lauretteroy <laurieroy.dev@gmail.com>
2020-07-18 01:34:56 -0600

    Add about, blog, contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I am new developer coming from the science world where I mostly
+                used Matlab,  and did some editing of files in JS, VB, python 
+                and sometimes managed networks. Working as a technician on 
+                Oceanographic research ships was engaging work that allowed me
+                to meet interesting people, travel to many places for work, and 
+                to enjoy water sports. 
+            </p>
+            <p>I enjoy learning and trying new things. I am now looking for a new place to live
+                and work. For the first time in a decade, it'll be away from the coast. I look forward
+                to more hiking and paddling! </p>
+        </div>
+    )
+}
+
+export default AboutPage
