7bd678e85e9199c50bdba913e1f71ada3a18b34c
lauretteroy <laurieroy.dev@gmail.com>
2020-07-22 19:26:21 -0600

    use graphQL playground

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,12 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+  })
 module.exports = {
   plugins: [
     'gatsby-plugin-sass'
   ],
   siteMetadata: {
-    title: 'Laurie Roy Gatsby Page', //decide what to name this
+    title: 'Laurie Roy',
     author: 'Laurie Roy'
   }
 }
