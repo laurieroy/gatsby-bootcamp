a263ce9a6020d476d15df0a64c64e4b2a58f7311
lauretteroy <laurieroy.dev@gmail.com>
2020-07-27 15:10:00 -0600

    Create slug for blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,15 @@
-module.exports.onCreateNode = ( node, actions) =>{
-const { createNodeField } = actions
-console.log(node)
-console.log(JSON.stringify(node, undefined, 4))
+const path = require('path')
+
+module.exports.onCreateNode = ({ node, actions }) => {    
+    const { createNodeField } = actions
+
+    if(node.internal.type == 'MarkdownRemark'){
+        const slug = path.basename(node.fileAbsolutePath, '.md')
+        
+        createNodeField({
+            node,
+            name: 'slug',
+            value: slug
+        })
+    }
 }
