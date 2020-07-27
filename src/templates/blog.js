8bbbf3716a1cdd94a7497c1705d87e0a0a8ebd64
lauretteroy <laurieroy.dev@gmail.com>
2020-07-27 15:14:39 -0600

    Add blog template

---
+++ src/templates/blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const blog = () => {
+    return (
+        <Layout>
+            This is blog template
+        </Layout>
+    )
+
+}
+
+export default Blog
