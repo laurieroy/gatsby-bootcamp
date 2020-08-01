5b000cf70eb4d49258f1e8a10eee645c1c032a91
lauretteroy <laurieroy.dev@gmail.com>
2020-08-01 15:48:37 -0600

    Add more styling to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { graphql, Link, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
+import blogStyles from './blog.module.scss'
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
@@ -25,10 +26,10 @@ const BlogPage = () => {
     return (
         <Layout>
             <h1>Blog Posts</h1>
-                <ol>
+                <ol className={blogStyles.posts}>
                     {data.allMarkdownRemark.edges.map((edge) => {
                         return (
-                            <li>
+                            <li className={blogStyles.post}>
                                 <Link to={`/blog/${edge.node.fields.slug}`}>
                                     <h2>{edge.node.frontmatter.title}</h2>
                                     <p>{edge.node.frontmatter.date}</p>
