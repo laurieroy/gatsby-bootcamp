3067ac4c8f80acf44c6eeb16e39003b418264964
lauretteroy <laurieroy.dev@gmail.com>
2020-08-03 14:30:02 -0600

    Add contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ require("dotenv").config({
   
 module.exports = {
   plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
     'gatsby-plugin-sass',
     {
       resolve: `gatsby-source-filesystem`,
